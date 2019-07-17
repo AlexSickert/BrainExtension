6ced0bd97b87e51d9ce248309caa918674afb581
alex@alexandersickert.com <1pipistrello>
2019-07-17 23:08:56 +0300

    get tags and convert to hashmap

--- Backend/src/Util/CheckUrl.java
+++ Backend/src/Util/CheckUrl.java
@@ -19,41 +19,43 @@ public class CheckUrl {
     
    
 
-    public static void checkUrl() {
-        try {
-            InetAddress inetAddress = InetAddress.getLocalHost();
+//    public static void checkUrl() {
+//        try {
+//            InetAddress inetAddress = InetAddress.getLocalHost();
+//
+//            inetAddress = InetAddress.getByName("www.ssl-id1.de");
+//            displayStuff("www.ssl-id1.de", inetAddress);
+//
+//            InetAddress[] inetAddressArray = InetAddress.getAllByName("www.ssl-id1.de");
+//            for (int i = 0; i < inetAddressArray.length; i++) {
+//                displayStuff("www.ssl-id1.de #" + (i + 1), inetAddressArray[i]);
+//            }
+//        } catch (UnknownHostException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
-            inetAddress = InetAddress.getByName("www.ssl-id1.de");
-            displayStuff("www.ssl-id1.de", inetAddress);
-
-            InetAddress[] inetAddressArray = InetAddress.getAllByName("www.ssl-id1.de");
-            for (int i = 0; i < inetAddressArray.length; i++) {
-                displayStuff("www.ssl-id1.de #" + (i + 1), inetAddressArray[i]);
-            }
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void displayStuff(String whichHost, InetAddress inetAddress) {
-        System.out.println("--------------------------");
-        System.out.println("Which Host:" + whichHost);
-        System.out.println("Canonical Host Name:" + inetAddress.getCanonicalHostName());
-        System.out.println("Host Name:" + inetAddress.getHostName());
-        System.out.println("Host Address:" + inetAddress.getHostAddress());
-    }
+//    private static void displayStuff(String whichHost, InetAddress inetAddress) {
+//        System.out.println("--------------------------");
+//        System.out.println("Which Host:" + whichHost);
+//        System.out.println("Canonical Host Name:" + inetAddress.getCanonicalHostName());
+//        System.out.println("Host Name:" + inetAddress.getHostName());
+//        System.out.println("Host Address:" + inetAddress.getHostAddress());
+//    }
 
     public static boolean checkAddress(HttpExchange he) {
         String x = he.getRemoteAddress().toString();
 
-        if (x.contains(acceptedAddress)) {
-            return true;
-        } else {
-            
-            Log.log("error", "acceptedAddress is " + acceptedAddress + "but address is " + x);
-            
-            return false;
-        }
+//        if (x.contains(acceptedAddress)) {
+//            return true;
+//        } else {
+//
+//            Log.log("error", "acceptedAddress is " + acceptedAddress + "but address is " + x);
+//
+//            return false;
+//        }
+
+        return true;
 
     }
 
